136068975b3c4566ee757378567c993aa508d442
Travis Hooper <thooper41@gmail.com>
2014-10-27 16:48:58 -0500

    website is running with minimal functionality

--- javascript/index_controller.js
+++ javascript/index_controller.js
@@ -1,5 +1,7 @@
+// myApp declaration
 var myApp = angular.module('myApp', ['ngRoute']);
 
+// routes
 myApp.config(function($routeProvider) {
 	$routeProvider
 
@@ -9,41 +11,38 @@ myApp.config(function($routeProvider) {
 		controller : 'homeCtrl'
 	})
 
-	.when('/subject', {
-		templateUrl : 'html/subject.html',
-		controller : 'subjectCtrl'
+	.when('/:subject', {
+		templateUrl : 'html/home.html',
+		controller : 'subCatCtrl'
 	})
 
-	.when('/calculator', {
+	.when('/:subject/:calc', {
 		templateUrl : 'html/calculator.html',
 		controller : 'calculatorCtrl'
+	})
+
+	.otherwise({
+		redirectTo: '/'
 	});
 });
 
-myApp.controller('dropDownCtrl', function($scope) {
-	$scope.subjects = subjects;
+// controllers
+
+
+myApp.controller('homeCtrl', function($rootScope) {
+	$("#wrapper").addClass('toggled');
+	$rootScope.shown = {};
 });
 
-myApp.controller('searchCtrl', function($scope) {
-	$scope.arr = ['ate', 'bad', 'snooze'];
+// populates dropdown menu
+myApp.controller('dropDownCtrl', function($rootScope) {
+	$rootScope.subjects = subjects;
 });
 
-myApp.controller('homeCtrl', function($scope) {
+// initializes search box
+myApp.controller('searchCtrl', function($rootScope) {
 
-});
-
-myApp.controller('subjectCtrl', function($scope) {
-
-});
-
-myApp.controller('calculatorCtrl', function($scope) {
-
-});
-
-
-$(document).on('ready', function () {
-
-	function populateSearchBox () {
+	(function () {
 		var availableSearch = [];
 		var findNames = function (obj) {
 			for (var key in obj) {
@@ -59,41 +58,85 @@ $(document).on('ready', function () {
 		$("#search-box").autocomplete({
 			source: availableSearch
 		});
-	}
-
-	populateSearchBox();
+	})();
 
 	$("#search-box-form").submit(function () {
-		var searchValue = $("#search-box").val(),
-		    searchObj;
+		var searchValue = $("#search-box").val().toLowerCase(),
+		searchObj, searchObjKey, searchObjRoute, searchSubCat;
 		
-		var findSubject = function (obj, name) {
+		var findSubject = function (obj, name, objKey, route, subCat) {
 			for (var key in obj) {
-				if ( typeof obj[key] === 'object') {
-					findSubject(obj[key], name);
-				} else if (obj.name === name) {
+				route = obj.route ? obj.route : route;
+				subCat = obj[key].calc ? key : subCat;
+				if ( typeof obj[key] === 'object' ) {
+					findSubject(obj[key], name, key, route, subCat);
+				} else if (obj.name.toLowerCase() === name) {
+					searchSubCat = subCat;
+					searchObjRoute = route;
+					searchObjKey = objKey;
 					searchObj = obj;
 					break;
 				}
 			}
 		};
-
 		findSubject(subjects, searchValue);
-
-		window.location.href = ("#/" + searchObj.route);
-
+		window.location.href = ("#/" + searchObjRoute);
+		$rootScope.subCat = searchSubCat;
+		console.log(searchSubCat);
+		if (!$rootScope.shown) {
+			$rootScope.shown = {};
+		}
+		$rootScope.shown[searchSubCat] = true;
 	});
-
 });
 
+myApp.controller('subCatCtrl', function($rootScope, $location) {
+	subjectObj = $location.$$path.split('/')[1];
+	if (!$rootScope.shown) {
+		$rootScope.shown = {};
+	}
+	$rootScope.hideAll = function () {
+		$rootScope.shown = {};
+	};
+	$rootScope.subjectObj = subjects[subjectObj];
+	$("#wrapper").removeClass("toggled");
+	$rootScope.toggleSubCat = function (key) {
+		$rootScope.shown[key] = $rootScope.shown[key] ? false : true;
+		$rootScope.subCat = key;
+	};
+	$rootScope.subCatState = function (key) {
+		return $rootScope.shown[key] ? undefined : 'toggled';
+	};
+});
 
-// angular.module('scopeExample', [])
-//   .controller('MyController', ['$scope', function($scope) {
-//     $scope.username = 'World';
+myApp.controller('calculatorCtrl', function($rootScope, $location) {
+	var calculatorObj = $location.$$path.split('/')[2];
+	var subjectObj = $location.$$path.split('/')[1];
+	$rootScope.answer = '';
+	$rootScope.subjectObj = subjects[subjectObj];
+	subCat = $rootScope.subCat;
+	$rootScope.calculatorObj = subjects[subjectObj].subCats[subCat].calc[calculatorObj];
+	$rootScope.myVars = {};
+	console.log($rootScope.myVars = {});
+	console.log($rootScope.calculatorObj.params.input);
+	$rootScope.showUnits = $rootScope.calculatorObj.params.units ? true : false;
+	$rootScope.output = function () {
+		for (var key in $rootScope.myVars) {
+			$rootScope.myVars[key] = parseInt($rootScope.myVars[key]);
+		}
+		$rootScope.answer = $rootScope.calculatorObj.func($rootScope.myVars);
+	};
+});
 
-//     $scope.sayHello = function() {
-//       $scope.greeting = 'Hello ' + $scope.username + '!';
-//     };
-//   }]);
-
+$(document).on('ready', function () {
+	$("#subject-dropdown, #search-box").on('click', function () {
+		$("#wrapper").addClass("toggled");
+	});
+	$(".dropdown-subject").on('click', function () {
+		$("#wrapper").removeClass("toggled");
+	});
+	$("#search-box-form").on('submit', function () {
+		$("#wrapper").removeClass("toggled");
+	});
+});
 
--- javascript/index_model.js
+++ javascript/index_model.js
@@ -1,31 +1,69 @@
 var subjects = {
 	conversions: {
 		name: "Conversions",
-		route: "subject",
+		route: "conversions",
 		subCats: {
+			currency: {
+				name: "Currency Conversions",
+				calc: {
+					allCurrency: {
+						name: "All Currency Conversions",
+						route: "conversions/allCurrency",
+					}
+				}
+			},
 			unit: {
 				name: "Unit Conversions",
-				route: "none",
 				calc: {
 					length: {
 						name: "Length Conversion",
-						route: "none",
+						route: "conversions/length",
+						params: {input: ['length'], output: ['length'], units:  [ 'centimeters', 'feet', 'inches', 'kilometer', 'meters', 'mile', 'milimeter', 'yard' ]},
+						func: function (hash) {},
 					}
 				}
-			}
+			},
+
 		}	
 	},
 	math: {
 		name: "Mathmatics",
-		route: "subject",
+		route: "math",
 		subCats: {
 			algebra: {
 				name: "Algebra",
-				route: "none",
 				calc: {
 					quadraticEquation: {
 						name: "Quadratic Equation",
-						route: "none",
+						route: "math/quadraticEquation",
+						params: {input: ['a','b','c'], output: ['x1', 'x2']},
+						func: function (hash) { var a = hash.a, b = hash.b, c = hash.c; var sol1 = (( -b + Math.pow(b * b - 4 * a * c  , 1/2)) / 2 * a); var sol2 = (( -b - Math.pow(b * b - 4 * a * c  , 1/2)) / 2 * a); return [sol1,sol2];},
+					}
+				}
+			},
+			calculus: {
+				name: "Calculus",
+				calc: {
+					derivative: {
+						name: "Derivative",
+						route: "math/derivative",
+					}
+				}
+			},
+			general: {
+				name: "General Math",
+				calc: {
+					addition: {
+						name: 'Addition',
+						route: 'math/addition',
+						params: {input: ['a','b'], output: ['sum']},
+						func: function (hash) {var a = hash.a, b = hash.b; return [a + b];},
+					},
+					squareRoot: {
+						name: 'Square Root',
+						route: "math/squareRoot",
+						params: {input: ['number'], output: ['square root']},
+						func: function (hash) { var num = hash.number; return [Math.pow(num, 1/2)];},
 					}
 				}
 			}
@@ -33,18 +71,32 @@ var subjects = {
 	},
 	physics: {
 		name: "Physics",
-		route: "subject",
+		route: "physics",
 		subCats: {
-			classcial: {
-				name: "Classical",
-				route: "none",
+			classical: {
+				name: "Classical Physics",
 				calc: {
+					force: {
+						name: "Force Calculation",
+						route: "physics/force",
+						params: {input: ['mass', 'acceleration'], output: ['force']},
+						func: function (hash) {var m = hash.mass, a = hash.acceleration; return [m*a];},
+					},
 					rotMomInert: {
 						name: "Rotational Moment of Inertia",
-						route: "none",
+						route: "physics/rotMomInert",
 					}
 				}
-			}
+			},
+			thermodynamics: {
+				name: "Thermodynamics",
+				calc: {
+					thermalCond: {
+						name: "Thermal Conductivity",
+						route: "physics/thermalCond",
+					}
+				}
+			},
 		}	
 	},
 };
